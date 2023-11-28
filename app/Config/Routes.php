759732660b026f299820881c2ab83d6b09fbadba
pranotoyogi <pranotoyogi05@gmail.com>
2023-11-29 00:41:07 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/about', 'Page::about');
+$routes->get('/contact', 'Page::education');
---
+++ app/Controllers/Page.php
@@ -0,0 +1,13 @@
+<?php namespace App\Controllers;
+
+class Page extends BaseController
+{
+	public function about()
+	{
+		echo "about page";
+	}
+    
+    public function contact()
+	{
+		echo "contact page";
+	}
